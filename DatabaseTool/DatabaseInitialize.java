8087275cc490fe14668d6ce22b48636ef6131c16
Anthony Bragg <braggac21@gmail.com>
2022-06-10 21:31:40 +0200

    Updated upload tool to work with new CSV

--- DatabaseTool/DatabaseInitialize.java
+++ DatabaseTool/DatabaseInitialize.java
@@ -230,15 +230,6 @@ class DatabaseInitialize {
                 ");"
             );
 
-            ReplaceTable("revenueHistory",
-                "  CREATE TABLE revenueHistory (" +
-                "  revenueDate DATE," +
-                "  revenue FLOAT," +
-                "  expenses FLOAT," +
-                "  profit FLOAT" +
-                ");"
-            );
-
             // Shares permissions of new tables with all of the development team
             ShareTable("currentinventory");
             ShareTable("product");
@@ -273,79 +264,6 @@ class DatabaseInitialize {
         }
     }
 
-    /* 
-        This function contains SQL commands to ensure that the database was set up correctly 
-    */
-    public static void PerformQueries(){
-        // User query #1
-        assert RunSelect("SELECT * FROM userinformation WHERE userID = 10;", "username") == "javier" : "Query U1 failed";
-
-        // User query #2
-        assert RunSelect("SELECT * FROM userinformation WHERE username = 'hugo';","userID") == "6" : "Query U2 failed";
-
-        // User query #3
-        assert RowCount("userinformation") == 10 : "Query U3 failed";
-
-        // Revenue History query #1
-        // RunQuery("");
-
-        // Revenue History query #2
-        // RunQuery("");
-
-        // Sales query #1
-        assert RunSelect("SELECT * FROM saleHistory WHERE saleId = 346;","saleDate") == "2022-06-09" : "Query S1 failed";
-
-        // Sales query #2
-        assert RunSelect("SELECT * FROM saleHistory WHERE saleID = 2;","revenue") == "43.88951363543289" : "Query S2 failed";
-
-        // Sales query #3
-        assert RowCount("saleHistory") == 401 : "Query S3 failed";
-
-        // Sale Items query #1
-        assert RunSelect("SELECT * FROM saleLineItem WHERE quantity = 1.1610749961068287;","saleID") == "149" : "Query L1 failed";
-
-        // Sale Items query #2
-        assert RunSelect("SELECT * FROM saleLineItem WHERE saleID = 207;","productID") == "17" : "Query L2 failed";
-
-        // Sale Items query #3
-        assert RowCount("saleLineItem") == 569 : "Query L3 failed";
-
-        // Current Inventory query #1
-        assert RunSelect("SELECT * FROM currentinventory WHERE productID = 53;","stockQuantity") == "91" : "Query C1 failed";
-
-        // Current Inventory query #2
-        assert RunSelect("SELECT * FROM currentinventory WHERE productID = 10","restockQuantity") == "142" : "Query C2 failed";
-
-        // Current Inventory query #3
-        assert RowCount("currentinventory") == 53 : "Query C3 failed";
-
-        // Vendor History query #1
-        assert RunSelect("SELECT * FROM vendorhistory WHERE saleID = 1","cost") == "2165.0" : "Query V1 failed";
-
-        // Vendor History query #2
-        assert RunSelect("SELECT * FROM vendorhistory WHERE saleDate = 2022-06-05","saleID") == "4" : "Query V2 failed";
-
-        // Vendor History query #3
-        assert RowCount("vendorhistory") == 6 : "Query V3 failed";
-
-        // Vendor Line Items query #1
-        assert RunSelect("SELECT * FROM vendorlineitem WHERE productID = 5","saleID") == "4" : "Query I1 failed";
-
-        // Vendor Line Items query #2
-        assert RunSelect("SELECT * FROM vendorlineitem WHERE productID = 27","quantity") == "50" : "Query I2 failed";
-
-        // Vendor Line Items query #3
-        assert RowCount("vendorlineitem") == 143 : "Query I3 failed";
-
-        // Product query #1
-        assert RunSelect("SELECT * FROM product WHERE productID = 53","productName") == "Yucca" : "Query P1 failed";
-
-        // Product query #2
-        assert RunSelect("SELECT * FROM product WHERE productID =15","sellPrice") == "2.15" : "Query P2 failed";
-        
-        // Product query #3
-        assert RowCount("product") == 53 : "Query P3 failed";
-    }
 
     public static void main(String[] args) {
         // Initialize connection for the database
@@ -364,15 +282,26 @@ class DatabaseInitialize {
 
         // Process data from csv file and run the associated SQL commands
         ProcessCSV("InitialDatabase/currentinventory.csv", "currentinventory", 53, 3);
-        ProcessCSV("InitialDatabase/product.csv", "product", 53, 6);
-        ProcessCSV("InitialDatabase/userinformation.csv", "userinformation", 10, 4);
-        ProcessCSV("InitialDatabase/salehistory.csv", "salehistory", 401, 3);
-        ProcessCSV("InitialDatabase/salelineitem.csv", "salelineitem", 569, 3);
-        ProcessCSV("InitialDatabase/vendorhistory.csv", "vendorhistory", 6, 3);
-        ProcessCSV("InitialDatabase/vendorlineitem.csv", "vendorlineitem", 143, 3);
+        System.out.println("Status Report: Current Inventory Table Completed and Populated");
 
-        // Verify database for correctness
-        PerformQueries();
+        ProcessCSV("InitialDatabase/product.csv", "product", 53, 6);
+        System.out.println("Status Report: Product Table Completed and Populated");
+
+        ProcessCSV("InitialDatabase/userinformation.csv", "userinformation", 10, 4);
+        System.out.println("Status Report: User Information Table Completed and Populated");
+
+        ProcessCSV("InitialDatabase/salehistory.csv", "salehistory", 409, 3);
+        System.out.println("Status Report: Sale History Table Completed and Populated");
+
+        ProcessCSV("InitialDatabase/salelineitem.csv", "salelineitem", 596, 3);
+        System.out.println("Status Report: Sale Line Item Table Completed and Populated");
+
+        ProcessCSV("InitialDatabase/vendorhistory.csv", "vendorhistory", 6, 3);
+        System.out.println("Status Report: Vendor History Table Completed and Populated");
+
+        ProcessCSV("InitialDatabase/vendorlineitem.csv", "vendorlineitem", 145, 3);
+        System.out.println("Status Report: Vendor Line Item Table Completed and Populated");
+
 
         // Close the connection for the database
         try {
