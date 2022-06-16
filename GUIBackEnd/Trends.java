042c1bff1e656802ff4968977196381b0077d628
carolineemejia <hi123484@tamu.edu>
2022-06-16 10:38:12 +0200

    Fixed the trends page layout, the button functions are a work in progress

--- GUIBackEnd/Trends.java
+++ GUIBackEnd/Trends.java
@@ -34,21 +34,54 @@ public class Trends extends JFrame {
         splitPane.setOrientation(SwingConstants.VERTICAL);
         setPreferredSize(new Dimension(600, 600));
 
+        /// RIGHT PANEL///
         // Adding elements to the right panel
-        JTextField startDate = new JTextField();
+        JTextField startDate = new JTextField("Start Date");
         startDate.setMaximumSize(new Dimension(200, 20));
-        // startDate.setBounds(100, 223, 100, 20);
 
-        JTextField endDate = new JTextField();
+        JTextField endDate = new JTextField("End Date");
         endDate.setMaximumSize(new Dimension(200, 20));
 
-        JButton enterButton = new JButton();
+        JButton enterButton = new JButton("Enter");
         enterButton.setMaximumSize(new Dimension(80, 20));
+
+        JButton homeButton = new JButton("Home");
+        homeButton.setPreferredSize(new Dimension(80, 20));
 
         rightPanel.add(startDate);
         rightPanel.add(endDate);
         rightPanel.add(enterButton);
+        rightPanel.add(Box.createRigidArea(new Dimension(50, 50)));
+        rightPanel.add(homeButton);
         rightPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        /// LEFT PANEL///
+        // Adding elements to the left panel
+        JTable table = new JTable();
+        table.setEnabled(false);
+
+        JButton salesReport = new JButton("Sales Report");
+        salesReport.setPreferredSize(new Dimension(200, 40));
+
+        JButton excessReport = new JButton("Excess Report");
+        excessReport.setPreferredSize(new Dimension(200, 40));
+
+        JButton restockReport = new JButton("Restock Report");
+        restockReport.setPreferredSize(new Dimension(200, 40));
+
+        JButton endOfDayReport = new JButton("End of Day Report");
+        endOfDayReport.setPreferredSize(new Dimension(200, 40));
+
+        // Adding a new panel into left panel for the buttons
+        JPanel subLeftPanel = new JPanel();
+
+        subLeftPanel.add(salesReport);
+        subLeftPanel.add(excessReport);
+        subLeftPanel.add(restockReport);
+        subLeftPanel.add(endOfDayReport);
+
+        leftPanel.add(table);
+        leftPanel.add(subLeftPanel);
 
         // Setting frame
         frame.add(splitPane);
