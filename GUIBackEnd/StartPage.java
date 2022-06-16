bdf8af869990b40a63c64a068eb89092ffce72fd
julianaLeano <julexd125@gmail.com>
2022-06-16 12:59:08 +0300

    ErrorMessage class updated to show error icon and started to implement StartPage class manager login popup, shows up but not fully implemented

--- GUIBackEnd/StartPage.java
+++ GUIBackEnd/StartPage.java
@@ -4,12 +4,11 @@ import java.awt.*;
 import javax.swing.*;
 
 public class StartPage extends JFrame {
-    // Initializing frame
-    private final JFrame frame;
 
     public StartPage() {
         // Create the main frame
-        frame = new JFrame("Start Page");
+        JFrame frame = new JFrame("Start Page");
+
 
         // Adding make a sale button
         JButton employeeButton = new JButton("Go to the Employee View");
@@ -25,8 +24,66 @@ public class StartPage extends JFrame {
         managerButton.setPreferredSize(new Dimension(200, 200));
         managerButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
-               new ManagerView();
-               frame.dispose();
+                JPanel managerPanel = new JPanel();
+                JTextField usernameField = new JTextField(20);
+                JPasswordField passwordField = new JPasswordField(20);
+
+                usernameField.setPreferredSize(new Dimension(300, 20));
+
+                managerPanel.add(new JLabel("Username: "));
+                managerPanel.add(usernameField);
+                managerPanel.add(new JLabel("Password: "));
+                managerPanel.add(passwordField);
+                managerPanel.setPreferredSize(new Dimension(300, 50));
+
+               int passwordPopup = JOptionPane.showConfirmDialog(frame, managerPanel, "Enter Manager Login", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+                
+               // after the OK option in the popup is selection, check the username and password from the database
+               if (passwordPopup == JOptionPane.OK_OPTION) {
+                    String usernameStr = new String(usernameField.getText());    
+                    String passwordStr = new String(passwordField.getPassword());
+                    
+                    // TO DO 
+                    //    - connect to database, get userinfo table
+                    //    - does username exist in database? 
+                    //       - exists? -> keep going
+                    //       - doesn't exist? -> incorrect username/password, go back to start page
+                    //    - does password match username?
+                    //       - matches? -> log in
+                    //       - doesn't match? -> incorrect username/password, go back to start page
+                    
+                    // --------------------------------------previous code --------------------------------------//
+                    /* 
+                    if (password.equals("yourmom")) {
+                        new ManagerView();
+                        frame.dispose();
+                    }
+
+                    else {
+                        JOptionPane.showMessageDialog(frame, "Incorrect Password. Please Try Again", "Error", JOptionPane.ERROR_MESSAGE);
+                    }
+                    
+                    */
+                    // --------------------------------------previous code --------------------------------------//
+                }
+               
+               
+               //---------------------------------------------------------------------------------------------------------//
+               /*  previous, just password, keep just in case
+               JPasswordField managerPassword = new JPasswordField();
+                int passwordPopup = JOptionPane.showConfirmDialog(frame, managerPassword, "Enter Manager Password", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+
+                if (passwordPopup == JOptionPane.OK_OPTION) {
+                    String password = new String(managerPassword.getPassword());
+                    if (password.equals("yourmom")) {
+                        new ManagerView();
+                        frame.dispose();
+                    }
+                    else {
+                        JOptionPane.showMessageDialog(frame, "Incorrect Password. Please Try Again", "Error", JOptionPane.ERROR_MESSAGE);
+                    }
+                }
+                */
             }
         });
 
